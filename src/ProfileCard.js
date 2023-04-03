3c56d541c19a55abfee44f85175160e8726981e7
asiimwe-a <tonyasiimwe14@gmail.com>
2023-04-04 01:54:23 +0300

    Add pda logos

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,9 @@
-function ProfileCard({ title, handle}) {
+function ProfileCard({ title, handle, image}) {
     // const { title, handle} = props;
-    
+
     return (
         <div>
+            <img src={image} alt="pda logo" />
             <div>Title is {title}</div>
             <div>Handle is {handle}</div>
         </div>
