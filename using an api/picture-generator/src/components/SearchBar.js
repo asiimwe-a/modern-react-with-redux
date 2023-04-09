e5041a152130453ab48677f06968c5ac25d3a35c
asiimwe-a <tonyasiimwe14@gmail.com>
2023-04-09 03:05:23 +0300

    Generate images and add styles

--- using an api/picture-generator/src/components/SearchBar.js
+++ using an api/picture-generator/src/components/SearchBar.js
@@ -1,12 +1,25 @@
-function SearchBar({ onSubmit }) {
-    const handleClick = () => {
-        onSubmit('cars');
-    }
+import './SearchBar.css';
+import { useState } from 'react';
+
+function SearchBar({ onSubmit}) {
+    const [term, setTerm] = useState('');
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        onSubmit(term);
+    };
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    };
 
     return (
-        <div>
-            <input />
-            <button onClick={handleClick}>Click me</button>
+        <div className='search-bar'>
+            <form onSubmit={handleFormSubmit}>
+            <label>Enter search term</label>
+                <input value={term} onChange={handleChange} />
+            </form>
         </div>
     );
 }
