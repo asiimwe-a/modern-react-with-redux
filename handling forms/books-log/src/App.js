041c1bf4636e2bd14703d7b03fac2d4955f3e370
asiimwe-a <tonyasiimwe14@gmail.com>
2023-04-26 23:55:31 +0300

    Update state fn for when a new book is added

--- handling forms/books-log/src/App.js
+++ handling forms/books-log/src/App.js
@@ -4,9 +4,14 @@ import BookCreate from './components/BookCreate';
 function App() {
     const [books, setBooks] = useState([]);
 
+    // const createBook = (title) => {
+    //     console.log('Need to add book with:', title);
+    // };
+
     const createBook = (title) => {
-        console.log('Need to add book with:', title);
-    };
+        const newBook = { title };
+        setBooks([...books, newBook]);
+      };
 
     return <div>
         <BookCreate onCreate={createBook} />
