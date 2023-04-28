b66004760263626bd5539a5478862b0082878b91
asiimwe-a <tonyasiimwe14@gmail.com>
2023-04-28 23:38:42 +0300

    Modify BookShow js code

--- handling forms/books-log/src/components/BookShow.js
+++ handling forms/books-log/src/components/BookShow.js
@@ -1,5 +1,25 @@
-function BookShow() {
-    return <div></div>;
+// function BookShow() {
+//     return <div></div>;
+// }
+
+// export default BookShow;
+
+import React from "react";
+
+function BookShow({ books }) {
+  return (
+    <div>
+      {books.length > 0 ? (
+        <ul>
+          {books.map((book, index) => (
+            <li key={index}>{book.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No books saved yet.</p>
+      )}
+    </div>
+  );
 }
 
 export default BookShow;
